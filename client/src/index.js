9b06147ea7ed71d35ad7533143963a60533e30e9
Sean Welch <sean.welch@me.com>
2022-01-28 14:42:46 -0800

    implemented routes and express server functionality. created placeholders for pages. added chakra and react router to main app wrapper

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { render } from 'react-dom';
-import App from './components/App.jsx';
 
-render(<App />, document.getElementById('app'));
+import App from './components/App.jsx';
+import Friends from './components/friends';
+import Events from './components/events';
+import Interests from './components/interests';
+import NotFound from './components/notfound';
+
+render(
+  <BrowserRouter>
+  <ChakraProvider>
+  <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="friends" element={<Friends />} />
+      <Route path="events" element={<Events />} />
+      <Route path="interests" element={<Interests />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
+  </ChakraProvider>
+  </BrowserRouter>
+  , document.getElementById('app'));
