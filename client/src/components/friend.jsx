e8ea1d70451b86568f79d9ba4802dd9da75b68fc
Andrew Lam <alam1324@gmail.com>
2022-01-28 15:51:26 -0800

    Update styling of friend entry

--- client/src/components/friend.jsx
+++ client/src/components/friend.jsx
@@ -1,8 +1,9 @@
+import { Flex } from '@chakra-ui/react';
 import React from 'react';
 
 const Friend = ( {friend} ) => {
   return (
-    <p>- {friend.full_name}</p>
+    <Flex p={4} m={4} border='1px' borderRadius='10px' borderColor='#8F8F8F'>{friend.full_name}</Flex>
   )
 }
 
