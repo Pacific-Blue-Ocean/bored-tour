6db50e6e28f708b53389ec2376093fcc43adb250
Andrew Lam <alam1324@gmail.com>
2022-01-27 17:41:58 -0800

    Initialize repo packages and folders

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# pacific
+# Bored Tour
+Helping people find things to do with their time.
+
+## Setup
+1. Install dependencies:
+```
+npm install
+```
+2. Setup Postgres database to match settings in `/database/index.js`.
+3. Rename `.env.example` to `.env` and fill in the enironment variables.
+4. Build scripts:
+```
+npm run build
+```
+5. Start the server:
+```
+npm start
+```
+6. Open the site:
+```
+http://localhost:3000
+```
+
+## Team
+- Andrew Lam
+- Cat Chiu
+- Eric Baldwin
+- James Song
+- Nanda Silva
+- Sean Welch
+- Yulan Rong
---
+++ database/index.js
@@ -0,0 +1,23 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: process.env.PG_USER,
+  host: 'localhost',
+  database: 'pacific',
+  port: 5432,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
+
+pool
+  .connect()
+  .then(() => console.log('Connected to Postgres database'))
+  .catch((err) => console.error('Error connecting to Postgres database', err.stack));
+
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle Postgres client', err);
+  process.exit(-1);
+});
+
+module.exports.pool = pool;
