f51c283a13781bf8e3c73a16a1876f7584a8a446
yulanrong <yulanrong123@gmail.com>
2022-01-28 21:17:46 -0800

    add files

---
+++ client/src/components/map.js
@@ -0,0 +1,10 @@
+import { Flex } from '@chakra-ui/react';
+import React from 'react';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+const Map = () => {
+
+};
+
+export default Map;
+
