1b766bc1d2b82a30586f1274b79128e654c2b4d9
Andrew Lam <alam1324@gmail.com>
2022-01-29 13:49:46 -0800

    Update query and styling

--- client/src/components/friends.js
+++ client/src/components/friends.js
@@ -2,7 +2,7 @@ import { Box, Flex, Heading, Button, Input, Stack } from '@chakra-ui/react';
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import { Header } from './header';
-import Friend from './friend.jsx';
+import Friend from './friends/friend.jsx';
 
 const Friends = () => {
   const [id, setUserId] = useState(1);
@@ -17,10 +17,12 @@ const Friends = () => {
   <Box>
     <Header />
     <Flex p={4} m={4} flexDirection="column" border='1px' borderRadius='10px' borderColor='#8F8F8F'>
-    <Heading>Friends</Heading>
+
+      <Heading>Friends</Heading>
+
       <Flex p={4} m={4} flexDirection="row">
-        <Input type="text" size='md' placeholder="Search/Add Friend" />
-        <Button type="submit">Submit</Button>
+        <Input type="text" size='md' placeholder="Friend Name" />
+        <Button type="submit">Add Friend</Button>
       </Flex>
       {friends.length ? friends.map((friend) => (
         <Friend
--- server/models.js
+++ server/models.js
@@ -2,17 +2,31 @@ const db = require('../database');
 
 const getFriends = (id) => {
   const query = `
-    select
+    select distinct
       u.*
-    from relationships r
-    join users u
-      on r.to_user_id = u.id
-    where r.from_user_id = $1;
+      ,case
+        when r.id is not null then true
+        else false
+      end as friend
+    from users u
+    left join relationships r
+      on u.id = r.from_user_id
+    where u.id <> $1
+    order by friend desc, u.id
   `
 
   return db.pool.query(query, [id]);
 }
 
+const addFriend = (id, friend_id) => {
+  const query = `
+    insert into relationships (from_user_id, to_user_id) values ($1, $2)
+  `
+
+  return db.pool.query(query, [id, friend_id]);
+}
+
 module.exports = {
   getFriends,
+  addFriend,
 };
