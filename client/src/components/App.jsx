dcd657391e4cee1842702d0d01e0fc3edf773b3c
yulanrong <yulanrong123@gmail.com>
2022-01-31 15:01:08 -0800

    fix time format

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,7 +2,7 @@ import { Heading, Stack, Button, FormControl, Flex } from '@chakra-ui/react';
 import React from 'react';
 import { Header } from './header';
 import { Link } from "react-router-dom";
-import homepage from '../../public/images/HomePage.jpeg';
+//import homepage from '../../public/images/HomePage.jpeg';
 import HomePage from './homepage/homepage.jsx';
 
 const App = () => {
--- client/src/components/eventDetail.js
+++ client/src/components/eventDetail.js
@@ -1,70 +1,137 @@
-import { Box, Grid, GridItem, Flex, Heading, Image, Text, Button } from '@chakra-ui/react';
-import React, { useState } from 'react';
-import { Header } from './header';
-import eventData from '../../../eventData.js';
-import Map from './map';
+import {
+  Box,
+  Grid,
+  GridItem,
+  Flex,
+  Heading,
+  Image,
+  Text,
+  Button,
+  Icon,
+  Spacer,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Header } from "./header";
+import eventData from "../../../eventData.js";
+import Map from "./map";
+import { MdFavoriteBorder } from "react-icons/md";
+import { MdFavorite } from "react-icons/md";
+import moment from 'moment';
 
-const EventDetail = () => {
+const EventDetail = ({ event_id }) => {
   const [event, setEvent] = useState(eventData);
-  const address = `${eventData.address_line1} ${eventData.address_city} ${eventData.address_state} ${eventData.address_zip}`;
+  const address = `${eventData.address_line1} ${eventData.address_state} ${eventData.address_zip}`;
 
   return (
     <Box>
       <Header />
-  <Grid
-      pl='5em'
-      pr='5em'
-      pt='2em'
-      h='auto'
-      templateRows='repeat(4, 1fr)'
-      templateColumns='repeat(2, 1fr)'
-      gap={5}>
-  <GridItem row={1} column={1} bg='red.300' align='center'>
-  <Image boxSize='400px'
-    objectFit='cover' src={eventData.mainPhoto} alt='event image' />
-  </GridItem>
-
-  <GridItem row={1} column={2} colSpan={1}>
-  <Heading as='h3' size='md'> {eventData.title} </Heading>
-  <Box>{eventData.price}</Box>
-  </GridItem>
-  <GridItem w="100%" h="20px" row={2} column={1}>
-    <Box h='20px'>
-      <Text>Fave</Text>
+      <Box pl="5em" pr="5em" pt="2em">
+        <Box>
+          <Flex>
+            <Image
+              boxSize="400px"
+              objectFit="cover"
+              align="center"
+              src={eventData.mainPhoto}
+              alt="event image"
+            />
+            <Spacer />
+            <Heading as="h3" size="md">
+              {" "}
+              {eventData.title}{" "}
+            </Heading>
+          </Flex>
+        </Box>
+        <Box>{eventData.price}</Box>
+        <Flex>
+          <Box>
+            <Icon as={MdFavoriteBorder} w={8} h={8} />
+          </Box>
+          <Spacer />
+          <Button
+            borderTopRadius="md"
+            align="center"
+            size="lg"
+            colorScheme="teal"
+            variant="solid"
+          >
+            {" "}
+            RSVP Now{" "}
+          </Button>
+        </Flex>
+        <Box>
+          <Text>{eventData.details}</Text>
+        </Box>
+        <Heading>Time:</Heading>
+        <Text>
+          {moment(eventData.date).format('MMMM Do YYYY')}, {eventData.start_time}
+        </Text>
+        <Heading>Duration:</Heading>
+        <Text>{eventData.event_length_minutes}min</Text>
+        <Heading>About the event:</Heading>
+        <Text>{eventData.description}</Text>
+      <Box>
+        <Heading>Location:</Heading>
+        <Text>{address}</Text>
+      </Box>
+      <Map address={address} />
     </Box>
-  </GridItem>
-
-  <GridItem w="100%" height='10px' row={2} column={2} align='center'>
-    <Box h='20px'>
-
-  <Button borderTopRadius="md" align='center' size='lg' colorScheme='teal' variant='solid'> RSVP Now </Button>
-    </Box>
-  </GridItem>
-
-  <GridItem rowSpan={3} column={1}>
-    <Box>
-    <Text>{eventData.details}</Text>
-    </Box>
-    <Heading>Time:</Heading>
-    <Text>{eventData.date} {eventData.start_time}</Text>
-    <Heading>Duration:</Heading>
-    <Text>{eventData.event_length_minutes}min</Text>
-    <Heading>About the event:</Heading>
-    <Text>{eventData.description}</Text>
-  </GridItem>
-
-  <GridItem row={3} column={2}>
-    <Box>
-    <Heading>Location:</Heading>
-    <Text>{address}</Text>
-    </Box>
-  </GridItem>
-  <GridItem row={4} column={2}>
-  <Map address={address} />
-  </GridItem>
-</Grid>
-  </Box>
+      </Box>
   );
 };
 
 export default EventDetail;
+
+{
+  /* <Grid
+    pl='5em'
+    pr='5em'
+    pt='2em'
+    templateRows='repeat(4, 1fr)'
+    templateColumns='repeat(2, 1fr)'
+    gap={5}>
+<GridItem row={1} column={1} bg='red.300' align='center'>
+<Image boxSize='400px'
+  objectFit='cover' align='center' src={eventData.mainPhoto} alt='event image' />
+</GridItem>
+
+<GridItem row={1} column={2} colSpan={1}>
+<Heading as='h3' size='md'> {eventData.title} </Heading>
+<Box>{eventData.price}</Box>
+</GridItem>
+<GridItem w="100%" h="20px" row={2} column={1}>
+  <Box h='20px'>
+  <Icon as={MdFavoriteBorder} />
+  </Box>
+</GridItem>
+
+<GridItem w="100%" height='10px' row={2} column={2} align='center'>
+  <Box h='20px'>
+
+<Button borderTopRadius="md" align='center' size='lg' colorScheme='teal' variant='solid'> RSVP Now </Button>
+  </Box>
+</GridItem>
+
+<GridItem rowSpan={3} column={1}>
+  <Box>
+  <Text>{eventData.details}</Text>
+  </Box>
+  <Heading>Time:</Heading>
+  <Text>{eventData.date} {eventData.start_time}</Text>
+  <Heading>Duration:</Heading>
+  <Text>{eventData.event_length_minutes}min</Text>
+  <Heading>About the event:</Heading>
+  <Text>{eventData.description}</Text>
+</GridItem>
+
+<GridItem row={3} column={2}>
+  <Box>
+  <Heading>Location:</Heading>
+  <Text>{address}</Text>
+  </Box>
+</GridItem>
+<GridItem row={4} column={2}>
+<Map address={address} />
+</GridItem>
+</Grid> */
+}
