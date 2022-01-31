95688e3f708b29e527011c6feb6d5bee8bf13b44
Andrew Lam <alam1324@gmail.com>
2022-01-31 10:41:03 -0800

    Show invite friend button

--- client/src/components/friends/friend.jsx
+++ client/src/components/friends/friend.jsx
@@ -2,10 +2,13 @@ import { Flex, Button } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import axios from 'axios';
 
-function Friend({ user_id, friend }) {
+function Friend({ user_id, friend, event_id }) {
   const [isFriend, setIsFriend] = useState(friend.friend);
 
-  const handleClick = (e) => {
+  // TODO: Check read from db if user is invited to event
+  const [isInvited, setIsInvited] = useState(false);
+
+  const handleFriendClick = (e) => {
     const request = e.target.value;
     const body = {
       user_id,
@@ -17,8 +20,19 @@ function Friend({ user_id, friend }) {
     } else if (request === 'remove') {
       axios.delete('/api/friends/', { data: body });
     }
-
     setIsFriend(!isFriend);
+  };
+
+  const handleInvite = (e) => {
+    const body = {
+      event_id,
+      friend_id: parseInt(friend.id, 10),
+    };
+
+    console.log(body);
+
+    axios.post('/api/events/invite', body);
+    setIsInvited(true);
   };
 
   return (
@@ -35,12 +49,19 @@ function Friend({ user_id, friend }) {
       {friend.location}
       <br />
       {isFriend ? (
-        <Button m={2} onClick={(e) => { handleClick(e); }} value="remove">
-          ❌ &nbsp; Remove
+        <Button m={2} onClick={handleFriendClick} value="remove">
+          ❌ &nbsp; Remove Friend
         </Button>
       ) : (
-        <Button m={2} onClick={handleClick} value="add">➕ &nbsp; Add</Button>
+        <Button m={2} onClick={handleFriendClick} value="add">
+          ➕ &nbsp; Add Friend
+        </Button>
       )}
+      {event_id && !isInvited && isFriend ? (
+        <Button m={2} onClick={handleInvite} value="invite">
+          ✉️ Invite to Event
+        </Button>
+      ) : null}
     </Flex>
   );
 }
