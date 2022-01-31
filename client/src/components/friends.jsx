ce415187aa95d3511ae8d9d6ca7f8e8d4a23150b
Andrew Lam <alam1324@gmail.com>
2022-01-31 12:52:51 -0800

    Implement searching for friends

--- client/src/components/friends.jsx
+++ client/src/components/friends.jsx
@@ -9,12 +9,28 @@ import Friend from './friends/friend.jsx';
 function Friends() {
   const [id, setUserId] = useState(1);
   const [friends, setFriends] = useState([]);
+  const [filteredFriends, setFilteredFriends] = useState([]);
   const [event_id, setEventId] = useState(5);
+  const [searchText, setSearchText] = useState('');
 
   useEffect(() => {
     axios.get('/api/friends', { params: { id } })
-      .then((res) => { setFriends(res.data); });
+      .then((res) => {
+        setFriends(res.data);
+        setFilteredFriends(res.data);
+      });
   }, [id]);
+
+  useEffect(() => {
+    let filteredFriends = friends.filter(friend => {
+      const friendName = friend.full_name.toLowerCase();
+      return friendName.includes(searchText.toLowerCase());
+    });
+
+    setFilteredFriends(filteredFriends);
+  }, [searchText])
+
+  const handleChange = (e) => { setSearchText(e.target.value); }
 
   return (
     <Box>
@@ -23,18 +39,17 @@ function Friends() {
 
         <Heading>Friends</Heading>
         <Flex p={4} m={8} flexDirection="row">
-          <Input type="text" placeholder="Search for a user" />
-          <Button>Search</Button>
+          <Input type="text" placeholder="Search for a user" value={searchText} onChange={handleChange} />
         </Flex>
         <Flex p={4} m={4} flexDirection="row" flexWrap="wrap">
-          {friends.length ? friends.map((friend) => (
+          {filteredFriends.length ? filteredFriends.map((friend) => (
             <Friend
               key={friend.id}
               user_id={id}
               friend={friend}
               event_id={event_id}
             />
-          )) : null}
+          )) : <span>No friends found</span>}
         </Flex>
       </Flex>
     </Box>
