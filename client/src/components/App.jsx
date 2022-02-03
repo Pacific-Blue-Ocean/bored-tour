416a1324590452a0739c956d12bd8ad5f01984c6
Catherine Chiu <chiuycatherine@gmail.com>
2022-02-02 19:55:24 -0800

    revert header in App.jsx for PR #77

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,19 +1,7 @@
 import { Header } from './header';
 import HomePage from './homepage/homepage.jsx';
 import React, { useState, useEffect } from "react";
-import {
-  Box, Heading, Spacer, Menu, Stack, FormControl, Input,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuItemOption,
-  MenuGroup,
-  MenuOptionGroup,
-  MenuDivider,
-  Button,
-  Flex,
-  extendTheme
-} from '@chakra-ui/react';
+import { Box, Input, Button, Flex} from '@chakra-ui/react';
 import { ChevronDownIcon } from '@chakra-ui/icons';
 import { Link } from "react-router-dom";
 import axios from 'axios';
@@ -38,25 +26,7 @@ const App = () => {
 
   return (
     <div>
-      <Flex w='100vw'>
-        <Link to="/">
-          <Heading as='h1' fontSize='6vh' h='11.5vh' color='brand.400' p={8}>bored tour</Heading>
-        </Link>
-        <Spacer />
-        <Box p={4}>
-        <Menu>
-          <MenuButton padding='1vw' fontSize='2.5vh' color='brand.400' as={Button} rightIcon={<ChevronDownIcon />}>
-          Username
-          </MenuButton>
-          <MenuList fontSize='2.5vh' color='brand.400'>
-            <Link to="/events"><MenuItem>My Events</MenuItem></Link>
-            <Link to="/friends"><MenuItem>My Friends</MenuItem></Link>
-            <Link to="/interests"><MenuItem>My Interests</MenuItem></Link>
-            <Button fontSize='2.5vh' color='brand.400' onClick={logout}> Log Out </Button>
-          </MenuList>
-        </Menu>
-        </Box>
-      </Flex>
+      <Header/>
       <Box
         backgroundImage="url('./images/RaccoonParty.jpeg')"
         backgroundSize='cover'
