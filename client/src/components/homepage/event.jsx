13282f6bc1dd3f477e55bbbfa3c2bfea639a5106
Andrew Lam <alam1324@gmail.com>
2022-02-05 09:27:20 -0800

    ESLint homepage/event.jsx

--- client/src/components/homepage/event.jsx
+++ client/src/components/homepage/event.jsx
@@ -1,41 +1,45 @@
-import { Spacer, GridItem, Image, Flex } from '@chakra-ui/react';
+import {
+  Spacer, GridItem, Image, Flex,
+} from '@chakra-ui/react';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Event = ({ event }) => {
+function Event({ event }) {
   return (
     <GridItem
-      fontSize='2vh'
-      textAlign='center'
-      justifyContent='center'
-      alignItems='center'
-      h='52vh'
-      marginLeft='2.5vw'
-      marginRight='2.5vw'
+      fontSize="2vh"
+      textAlign="center"
+      justifyContent="center"
+      alignItems="center"
+      h="52vh"
+      marginLeft="2.5vw"
+      marginRight="2.5vw"
     >
       <Flex
-        alignItems='center'
-        justifyContent='space-around'
-        w='32vh'
-        fontWeight='bold'
+        alignItems="center"
+        justifyContent="space-around"
+        w="32vh"
+        fontWeight="bold"
       >
-        <Link to={`/events/${event.id}`} style={{textDecoration: 'none'}}>
+        <Link to={`/events/${event.id}`} style={{ textDecoration: 'none' }}>
           <Image
             src={event.mainphoto}
-            align='center'
-            w='32vh'
-            h='40vh'
-            objectFit='cover'
-            borderRadius='5%'
-            marginBottom='0.5vw'
+            align="center"
+            w="32vh"
+            h="40vh"
+            objectFit="cover"
+            borderRadius="5%"
+            marginBottom="0.5vw"
           />
           {event.title}
-          <Spacer/>
-          $ {event.price}
+          <Spacer />
+          $
+          {' '}
+          {event.price}
         </Link>
       </Flex>
     </GridItem>
-  )
+  );
 }
 
 export default Event;
